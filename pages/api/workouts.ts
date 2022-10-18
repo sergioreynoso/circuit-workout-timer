27e8133e899d9fcd2cce2b7e8ea08442733f5125
Sergio Reynoso <sergior@gmail.com>
2022-10-18 10:15:42 -0400

    feat: add workouts list page

---
+++ pages/api/workouts.ts
@@ -0,0 +1,34 @@
+import prisma from "../../lib/prisma";
+import { NextApiRequest, NextApiResponse } from "next";
+import { Workouts } from "@prisma/client";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "GET") {
+    try {
+      const workouts = await prisma?.workouts.findMany({
+        where: {
+          userId: req.query.id as string,
+        },
+      });
+      res.status(200).json(workouts);
+    } catch (error) {
+      res.status(400).json({ message: "Something went wrong" });
+    }
+  }
+  if (req.method === "POST") {
+    try {
+      // await prisma.note.create({
+      //   data: {
+      //     title,
+      //     content,
+      //   },
+      // });
+      res.status(200).json({ message: "Workout Created" });
+    } catch (error) {
+      res.status(400).json({ message: "Something went wrong" });
+    }
+  }
+}
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,7 +1,86 @@
+import { Workouts } from "@prisma/client";
+import { useQuery } from "@tanstack/react-query";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
 import React from "react";
+import Button from "../components/button";
+import prisma from "../lib/prisma";
+import { styled } from "../styles/stitches.congif";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const { data: session, status } = useSession();
+  const workoutQuery = useQuery(["workouts"], () =>
+    fetch(`api/workouts?id=${session?.user?.id}`).then((res) => res.json())
+  );
+
+  if (workoutQuery.isLoading) {
+    return (
+      <Flex
+        css={{
+          justifyContent: "center",
+          padding: "24px",
+        }}>
+        <p>Loading...</p>
+      </Flex>
+    );
+  }
+
+  const workouts: Workouts[] = workoutQuery.data;
+
+  return (
+    <Flex
+      as="main"
+      css={{
+        alignItems: "center",
+        paddingTop: "24px",
+      }}>
+      <UList>
+        {workouts.map((work) => {
+          return (
+            <List as="li" key={work.id}>
+              <Link href={`/workout/${work.id}`}>
+                <LinkTag>
+                  <Label> {work.workout_name}</Label>
+                </LinkTag>
+              </Link>
+            </List>
+          );
+        })}
+      </UList>
+    </Flex>
+  );
 };
+
+const Flex = styled("div", {
+  display: "flex",
+  justifyContent: "center",
+});
+
+const UList = styled("ul", {
+  flex: 1,
+  maxWidth: "800px",
+  display: "flex",
+  flexDirection: "column",
+  gap: "16px",
+  listStyle: "none",
+});
+
+const List = styled("li", {
+  height: "50px",
+  backgroundColor: "$primary-03",
+});
+
+const Label = styled("div", {
+  display: "flex",
+
+  alignItems: "center",
+  height: "100%",
+  paddingLeft: "24px",
+});
+
+const LinkTag = styled("a", {
+  textDecoration: "none",
+  cursor: "pointer",
+});
 
 export default Dashboard;
