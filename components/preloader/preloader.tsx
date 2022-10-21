d8ab8e2e40e2cbcab72f2078ed2c7b951dec0eb8
Sergio Reynoso <sergior@gmail.com>
2022-10-21 09:55:10 -0400

    feat: add preloader component

--- components/preloader/preloader.tsx
+++ components/preloader/preloader.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { styled } from "../../styles/stitches.congif";
 
 const Preloader = ({ label = "preloader" }: { label: string }) => {
-  return <Plex>{label}</Plex>;
+  return <Box>{label}</Box>;
 };
 
 export default Preloader;
 
-const Plex = styled("div", {
+const Box = styled("div", {
   display: "flex",
+  justifyContent: "center",
   height: "100%",
+  padding: "24px",
 });
