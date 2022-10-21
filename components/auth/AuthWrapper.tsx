5976374c6c42303233bb9ec3886b10c9035a3b4c
Sergio Reynoso <sergior@gmail.com>
2022-10-21 09:56:41 -0400

    chore: add index file to AuthWrapper

--- components/auth/AuthWrapper.tsx
+++ components/auth/AuthWrapper.tsx
@@ -1,7 +1,7 @@
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import React from "react";
-import ProtectedRoutes from "./ProtectedRoutes";
+import ProtectedRoutes from "./protectedRoutes";
 
 const authRoutes = ["/dashboard"];
 
