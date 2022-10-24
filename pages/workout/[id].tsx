e2bf046e77ee343182356ade81c236d42a148efa
Sergio Reynoso <sergior@gmail.com>
2022-10-24 13:19:19 -0400

    refactor: use custom hooks to load workout and exercise
    Workout and Exercise are not queried with their own respective hooks.

--- pages/workout/[id].tsx
+++ pages/workout/[id].tsx
@@ -3,41 +3,43 @@ import { useRouter } from "next/router";
 import { styled } from "../../styles/stitches.congif";
 import Preloader from "../../components/preloader";
 import Timer from "../../components/timer";
-import useLoadWorkout from "../../hooks/useLoadWorkout";
+
 import Link from "next/link";
+import useWorkoutQuery from "../../hooks/useWorkoutQuery";
+import useExerciseQuery from "../../hooks/useExerciseQuery";
 
 const Workout = () => {
   const router = useRouter();
   const { id } = router.query;
-  const [workoutQuery, exerciseQuery] = useLoadWorkout(id as string);
+
+  const workoutQuery = useWorkoutQuery(id as string);
+  const exerciseQuery = useExerciseQuery(id as string);
 
   if (workoutQuery.isLoading && exerciseQuery.isLoading)
     return <Preloader label="Loading..." />;
 
-  if (workoutQuery.error)
+  if (workoutQuery.error || !workoutQuery.isSuccess)
     return <Preloader label={`Error loading workout: ${workoutQuery.error}`} />;
 
-  if (exerciseQuery.error)
+  if (exerciseQuery.error || !exerciseQuery.isSuccess)
     return (
       <Preloader label={`Error loading exercise: ${exerciseQuery.error}`} />
     );
 
-  if (workoutQuery.isSuccess && exerciseQuery.isSuccess) {
-    return (
-      <Flex>
-        <Heading1>{workoutQuery.data.workout_name}</Heading1>
-        <Link href={`/edit_workout/${id as string}`}>
-          <Edit>Edit</Edit>
-        </Link>
-        <Flex>
-          <Timer workout={workoutQuery.data} exercises={exerciseQuery.data} />
-        </Flex>
-      </Flex>
-    );
-  }
+  return (
+    <Box>
+      <Heading1>{workoutQuery.data.workout_name}</Heading1>
+      <Link href={`/editWorkout/${id as string}`}>
+        <Edit>Edit</Edit>
+      </Link>
+      <Box>
+        <Timer workout={workoutQuery.data} exercises={exerciseQuery.data} />
+      </Box>
+    </Box>
+  );
 };
 
-const Flex = styled("div", {
+const Box = styled("div", {
   display: "flex",
   flexDirection: "column",
   alignItems: "center",
