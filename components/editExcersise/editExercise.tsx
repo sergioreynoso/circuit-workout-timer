0c0a14dc9eecf491948a263b815b97b3e4032ed7
Sergio Reynoso <sergior@gmail.com>
2022-10-26 13:20:48 -0400

    refactor: move dialog it's own reusable component

--- components/editExcersise/editExercise.tsx
+++ components/editExcersise/editExercise.tsx
@@ -1,93 +1,97 @@
-import * as Dialog from "@radix-ui/react-dialog";
 import React, { useState } from "react";
-import { keyframes, styled } from "../../styles/stitches.congif";
-import { Cross2Icon } from "@radix-ui/react-icons";
+
 import EditExerciseForm from "../editExerciseForm";
 import { ExerciseWithTimestamp } from "../../hooks/useWorkout";
+import { DialogTitle } from "@radix-ui/react-dialog";
+import Dialog from "../dialog";
+import { styled } from "../../styles/stitches.congif";
+import Input from "../input";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+import { Exercise } from "@prisma/client";
 import Button from "../button";
 
-const EditExercise = ({
-  exerciseData,
-}: {
+type Props = {
   exerciseData: ExerciseWithTimestamp;
-}) => {
-  const [open, setOpen] = useState(false);
+  children?: JSX.Element;
+};
+
+const EditExercise = ({ exerciseData, children }: Props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const queryClient = useQueryClient();
+  const mutation = useMutation(
+    (exercise: Omit<Exercise, "workoutId">) =>
+      axios.post("/api/exercise", exercise),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(["exercises"]);
+        setIsOpen(false);
+      },
+    }
+  );
+
+  const [inputValue, setInputValue] = useState({
+    name: exerciseData.exercise_name,
+    duration: Math.round(exerciseData.duration / 1000),
+    type: exerciseData.type,
+  });
+  const { name, duration, type } = inputValue;
+
+  const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const { name, value } = e.currentTarget;
+    setInputValue((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const onFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    mutation.mutate({
+      id: exerciseData.id,
+      exercise_name: name,
+      type: type,
+      duration: Number(duration * 1000),
+    });
+  };
 
   return (
-    <Dialog.Root open={open} onOpenChange={setOpen}>
-      <Dialog.Trigger asChild>
-        <Button color="violet">Edit</Button>
-      </Dialog.Trigger>
-      <Dialog.Portal>
-        <Overlay />
-        <Content>
-          <Dialog.Title>Edit Exercise</Dialog.Title>
-          {/* <Dialog.Description>Edit your exercise here.</Dialog.Description> */}
-          <EditExerciseForm exerciseData={exerciseData} setOpen={setOpen} />
-          <Dialog.Close asChild>
-            <IconButton aria-label="Close">
-              <Cross2Icon />
-            </IconButton>
-          </Dialog.Close>
-        </Content>
-      </Dialog.Portal>
-    </Dialog.Root>
+    <Dialog label="Edit" isOpen={isOpen} setIsOpen={setIsOpen}>
+      <DialogTitle>Edit Exercise</DialogTitle>
+      <Wrapper as="form" css={{ gap: "$xl" }} onSubmit={onFormSubmit}>
+        <Input
+          type="text"
+          label="Exercise Name"
+          name="name"
+          value={name}
+          onChange={handleChange}
+          placeholder=""
+        />
+        <Input
+          type="number"
+          label="duration"
+          name="duration"
+          value={duration}
+          onChange={handleChange}
+          placeholder=""
+        />
+        <div>{mutation.isLoading && "Updating exercise..."}</div>
+        <Button color="violet" type="submit">
+          Update Workout
+        </Button>
+      </Wrapper>
+    </Dialog>
   );
 };
 
-const overlayShow = keyframes({
-  "0%": { opacity: 0 },
-  "100%": { opacity: 1 },
-});
-
-const contentShow = keyframes({
-  "0%": { opacity: 0, transform: "translate(-50%, -48%) scale(.96)" },
-  "100%": { opacity: 1, transform: "translate(-50%, -50%) scale(1)" },
-});
-
-const Content = styled(Dialog.Content, {
-  backgroundColor: "$primary-01",
-  borderRadius: 6,
-  boxShadow:
-    "hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px",
-  position: "fixed",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: "90vw",
-  maxWidth: "450px",
-  padding: 25,
-  "@media (prefers-reduced-motion: no-preference)": {
-    animation: `${contentShow} 150ms cubic-bezier(0.16, 1, 0.3, 1)`,
-  },
-  "&:focus": { outline: "none" },
-});
-
-const Overlay = styled(Dialog.Overlay, {
-  backgroundColor: "$grayA-09",
-  position: "fixed",
-  inset: 0,
-  "@media (prefers-reduced-motion: no-preference)": {
-    animation: `${overlayShow} 150ms cubic-bezier(0.16, 1, 0.3, 1)`,
-  },
-});
-
-const IconButton = styled("button", {
-  all: "unset",
-  fontFamily: "inherit",
-  borderRadius: "100%",
-  height: 25,
-  width: 25,
-  display: "inline-flex",
+const Wrapper = styled("div", {
+  display: "flex",
+  flexDirection: "column",
   alignItems: "center",
   justifyContent: "center",
-  color: "$primary-12",
-  position: "absolute",
-  top: 10,
-  right: 10,
-
-  "&:hover": { backgroundColor: "$primary-03" },
-  "&:focus": { boxShadow: `0 0 0 2px $colors$primary-09` },
+  padding: "24px",
 });
 
 export default EditExercise;
