fd06bbe85ad0736a4ed7ec53a0860e2abf91e68b
Sergio Reynoso <sergior@gmail.com>
2022-10-27 12:13:25 -0400

    add type utility Omit to Exercise

--- hooks/useWorkout.ts
+++ hooks/useWorkout.ts
@@ -2,11 +2,11 @@ import { Exercise, Workout } from "@prisma/client";
 import { useMemo } from "react";
 import { addTimestamp } from "../lib/addTimestamp";
 
-export interface ExerciseWithTimestamp extends Exercise {
+export interface ExerciseWithTimestamp extends Omit<Exercise, "display_seq"> {
   timestamp?: { start: number; end: number };
 }
 
-const warmUp: Exercise = {
+const warmUp: ExerciseWithTimestamp = {
   id: "",
   type: "REST",
   exercise_name: "Warm up",
