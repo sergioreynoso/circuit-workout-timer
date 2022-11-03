21d384e6254008b834e7c76805f4d8b6b1522520
Sergio Reynoso <sergior@gmail.com>
2022-11-03 14:31:11 -0400

    refactor: move exercise mutation to own end point

--- pages/api/updateExercise.ts
+++ pages/api/updateExercise.ts
@@ -7,16 +7,17 @@ export default async function handler(
 ) {
   if (req.method === "POST") {
     try {
-      const exercise = await prisma?.exercise.create({
+      const workout = await prisma?.exercise.update({
+        where: {
+          id: req.body.id,
+        },
         data: {
           exercise_name: req.body.exercise_name,
           type: req.body.type,
           duration: req.body.duration,
-          display_seq: req.body.display_seq,
-          workoutId: req.body.workoutId,
         },
       });
-      res.status(200).json(exercise);
+      res.status(200).json(workout);
     } catch (error) {
       res
         .status(400)
