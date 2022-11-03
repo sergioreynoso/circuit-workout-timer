b18c1f50a1345005de587259a0578dcbb6061b35
Sergio Reynoso <sergior@gmail.com>
2022-11-03 14:30:17 -0400

    refactor: removed post method
    Post method is move to it's own api route.

--- pages/api/workout.ts
+++ pages/api/workout.ts
@@ -25,24 +25,4 @@ export default async function handler(
       res.status(400).json({ message: "Something went wrong getting workout" });
     }
   }
-
-  if (req.method === "POST") {
-    try {
-      const workout = await prisma?.workout.update({
-        where: {
-          id: req.body.id,
-        },
-        data: {
-          workout_name: req.body.workout_name,
-          set_count: req.body.set_count,
-          set_rest: req.body.set_rest,
-        },
-      });
-      res.status(200).json(workout);
-    } catch (error) {
-      res
-        .status(400)
-        .json({ message: "Something went wrong updating workout." });
-    }
-  }
 }
