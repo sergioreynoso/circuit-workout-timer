60a69da279d211c1a4ef3d46e7d262cfbf87588f
Sergio Reynoso <sergior@gmail.com>
2022-11-03 14:31:57 -0400

    refactor: removed unneeded styles components

--- components/workoutList/workoutList.tsx
+++ components/workoutList/workoutList.tsx
@@ -2,6 +2,9 @@ import Link from "next/link";
 import React from "react";
 import useAllWorkoutsQuery from "../../hooks/useAllWorkoutsQuery";
 import { styled } from "../../styles/stitches.congif";
+import Button from "../button";
+import DeleteWorkoutDialog from "../deleteWorkoutDialog";
+import { Flex } from "../layout";
 import Preloader from "../preloader";
 
 const WorkoutList = ({ userId }: { userId: string }) => {
@@ -11,35 +14,34 @@ const WorkoutList = ({ userId }: { userId: string }) => {
   if (error || !isSuccess) return <Preloader label={`Error:${error}`} />;
 
   return (
-    <ListWrapper>
+    <Flex
+      direction="column"
+      css={{
+        gap: "$sm",
+        listStyle: "none",
+      }}>
       {data.map((workout) => {
         return (
-          <List key={workout.id}>
+          <Flex
+            as="li"
+            key={workout.id}
+            css={{
+              justifyContent: "space-between",
+              height: "50px",
+              backgroundColor: "$primary-03",
+            }}>
             <Link href={`/workout/${workout.id}`}>
               <LinkTag>
                 <Label> {workout.workout_name}</Label>
               </LinkTag>
             </Link>
-          </List>
+            <DeleteWorkoutDialog workoutId={workout.id} />
+          </Flex>
         );
       })}
-    </ListWrapper>
+    </Flex>
   );
 };
-
-const ListWrapper = styled("ul", {
-  flex: 1,
-  maxWidth: "800px",
-  display: "flex",
-  flexDirection: "column",
-  gap: "16px",
-  listStyle: "none",
-});
-
-const List = styled("li", {
-  height: "50px",
-  backgroundColor: "$primary-03",
-});
 
 const Label = styled("div", {
   display: "flex",
