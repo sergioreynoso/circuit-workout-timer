d24f47051f8367f27fab4975ae9b3a2e0ae71496
Sergio Reynoso <sergior@gmail.com>
2022-11-04 22:33:03 -0400

    refactor: add type narrowing and reject promises on error

--- hooks/useFetchWorkouts.ts
+++ hooks/useFetchWorkouts.ts
@@ -1,22 +1,33 @@
-import { Workout } from "@prisma/client";
+import { Exercise, Workout } from "@prisma/client";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 type Endpoint = "getWorkout" | "getAllWorkouts";
 
+const fetchWorkout = (
+  id: string | undefined,
+  endPoint: string | undefined
+): Promise<Workout | Workout[]> => {
+  if (typeof id === "undefined")
+    return Promise.reject(new Error("Invalid id."));
+  if (typeof endPoint === "undefined")
+    return Promise.reject(new Error("Invalid API end point."));
+
+  return axios
+    .get(`/api/${endPoint}`, { params: { id: id } })
+    .then((res) => res.data);
+};
+
 const useFetchWorkouts = (
-  endpoint: Endpoint,
-  id: string,
+  endPoint: Endpoint,
+  id: string | undefined,
   initialData: Workout | Workout[] | undefined = undefined
 ) => {
-  const fetchWorkout = (id: string): Promise<Workout[]> =>
-    axios
-      .get(`/api/${endpoint}`, { params: { id: id } })
-      .then((res) => res.data);
+  const uniqueKey: string = endPoint === "getWorkout" ? "workout" : "workouts";
 
   return useQuery({
-    queryKey: ["workouts", id],
-    queryFn: () => fetchWorkout(id),
+    queryKey: [uniqueKey, id, endPoint],
+    queryFn: () => fetchWorkout(id, endPoint),
     initialData: initialData,
   });
 };
