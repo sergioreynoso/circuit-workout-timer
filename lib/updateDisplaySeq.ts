bf9874614799c1354d7d1e647279a9aa985c743d
Sergio Reynoso <sergior@gmail.com>
2022-11-15 16:19:07 -0500

    refactor: remove mutation from function
    mutation is now down on the server

--- lib/updateDisplaySeq.ts
+++ lib/updateDisplaySeq.ts
@@ -5,11 +5,6 @@ const updateDisplaySeq = <T extends Exercise | Workout>(
   mutation: any
 ): T[] => {
   return items.map((item, index) => {
-    mutation.mutate({
-      id: item.id,
-      display_seq: index,
-    });
-
     return { ...item, display_seq: index };
   });
 };
