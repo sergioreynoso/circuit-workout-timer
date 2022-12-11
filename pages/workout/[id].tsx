cd9dd7be66e48efa937796a158986b75110576e4
Sergio Reynoso <sergior@gmail.com>
2022-12-11 18:05:09 -0500

    refactor: flatten Timer component tree
    Remove Timer and move components to parent to avoid  necessary proper drilling.

--- pages/workout/[id].tsx
+++ pages/workout/[id].tsx
@@ -1,16 +1,23 @@
 import React from "react";
 import { styled } from "../../styles/stitches.congif";
-import Timer from "../../components/timer";
 import { prisma } from "../../lib/prisma";
 import { GetServerSideProps } from "next";
 import { WorkoutWithExercises } from "../../hooks/useFetchWorkout";
 import { Flex } from "../../components/layout";
+import { useWorkout } from "../../hooks/useWorkout";
+import useTimer from "../../hooks/useTimer";
+import Timer_control from "../../components/timerControl";
+import Exercise_counter from "../../components/exerciseCounter";
 
 type WorkoutTimerProps = {
   initialData: WorkoutWithExercises;
 };
 
 const WorkoutTimer = ({ initialData }: WorkoutTimerProps) => {
+  const [workoutExercises, workoutTotalTime] = useWorkout(initialData);
+  const [remainingTime, isTimerRunning, isTimerDone, toggleTimer] =
+    useTimer(workoutTotalTime);
+
   return (
     <Flex
       direction="column"
@@ -20,9 +27,23 @@ const WorkoutTimer = ({ initialData }: WorkoutTimerProps) => {
         padding: "24px",
       }}>
       <Heading1>{initialData.workout_name}</Heading1>
-
       <Flex css={{ flex: "1" }}>
-        <Timer workoutDetails={initialData} exercises={initialData.exercises} />
+        <Flex
+          as="main"
+          css={{ flexDirection: "column", alignItems: "center", gap: "$2x" }}>
+          <Exercise_counter
+            workoutExercises={workoutExercises}
+            remainingTime={remainingTime}
+          />
+          <Footer>
+            <Timer_control
+              workoutId={initialData.id}
+              toggleTimer={toggleTimer}
+              isTimerRunning={isTimerRunning}
+              isTimerDone={isTimerDone}
+            />
+          </Footer>
+        </Flex>
       </Flex>
     </Flex>
   );
@@ -52,6 +73,21 @@ const Heading1 = styled("h1", {
   lineHeight: "$150",
   textAlign: "center",
   marginBottom: "$lg",
+});
+
+const Footer = styled("div", {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  gap: "$lg",
+  width: "100%",
+  height: "80px",
+  paddingInline: "$2x",
+  backgroundColor: "$primary-02",
+  "@less-sm": {
+    position: "fixed",
+    bottom: "0",
+  },
 });
 
 export default WorkoutTimer;
