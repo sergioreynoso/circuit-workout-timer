2ac46f0cb2c7df87f808e888fc744b0cc9b0265b
Sergio Reynoso <sergior@gmail.com>
2022-12-11 14:20:21 -0500

    refactor: reduce multiple useMemos to one
    Optimize code by reducing the number of useMemo from e to 1.
    
    Code is clear to read.

--- hooks/useWorkout.ts
+++ hooks/useWorkout.ts
@@ -1,53 +1,51 @@
 import { Exercise, Workout } from "@prisma/client";
 import { useMemo } from "react";
 import { addTimestamp } from "../lib/addTimestamp";
+import range from "lodash/range";
+import cuid from "cuid";
 
 export interface ExerciseWithTimestamp extends Omit<Exercise, "display_seq"> {
   timestamp?: { start: number; end: number };
 }
 
-const warmUp: ExerciseWithTimestamp = {
-  id: "",
-  type: "REST",
+const WARMUP: ExerciseWithTimestamp = {
+  id: cuid(),
   exercise_name: "Warm up",
+  type: "REST",
   duration: 10000,
   workoutId: "",
 };
 
+const SET_REST = (rest: number): ExerciseWithTimestamp => ({
+  id: cuid(),
+  exercise_name: "Set Rest",
+  type: "REST",
+  duration: rest,
+  workoutId: "",
+});
+
 export function useWorkout(
-  workout: Workout,
+  workoutDetails: Workout,
   exercises: ExerciseWithTimestamp[]
 ): [ExerciseWithTimestamp[], number] {
-  const totalSets = workout.set_count;
-  const setRest = workout.set_rest;
+  const totalSets = workoutDetails.set_count;
+  const setRest = workoutDetails.set_rest;
 
-  let workoutExercises = useMemo(() => {
-    let array = [warmUp];
-    if (exercises) {
-      for (let i = 0; i < totalSets; i++) {
-        array.push(...exercises);
-        //Add set-rest after end of set
-        if (i < totalSets - 1)
-          array.push({
-            type: "REST",
-            id: "",
-            exercise_name: "Set Rest",
-            duration: setRest,
-            workoutId: "",
-          });
-      }
-    }
+  return useMemo(() => {
+    //Formats workout of sets and exercises
+    let workout = range(totalSets).flatMap((_, index) => {
+      let arr = [...exercises];
+      if (index === 0) arr.unshift(WARMUP);
+      if (index < totalSets - 1) arr.push(SET_REST(setRest));
+      return arr;
+    });
 
-    return array;
+    const workoutTotalTime = workout.reduce((prev, curr) => {
+      return prev + curr.duration;
+    }, 0);
+
+    workout = addTimestamp(workout, workoutTotalTime);
+
+    return [workout, workoutTotalTime];
   }, [exercises, totalSets, setRest]);
-
-  const workoutTotalTime = useMemo(() => {
-    return workoutExercises.reduce((prev, curr) => prev + curr.duration, 0);
-  }, [workout]);
-
-  workoutExercises = useMemo(() => {
-    return addTimestamp(workoutExercises, workoutTotalTime);
-  }, [workoutExercises, workoutTotalTime]);
-
-  return [workoutExercises, workoutTotalTime];
 }
