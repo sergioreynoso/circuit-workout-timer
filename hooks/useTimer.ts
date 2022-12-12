e933a8615d403bebdd9371960a0fb1c8605b9074
Sergio Reynoso <sergior@gmail.com>
2022-12-11 22:15:10 -0500

    refactor: move setInterval to useRef
    SetInterval can be cleared from outside useEffect.
    
    Avoids setting an empty variable.

--- hooks/useTimer.ts
+++ hooks/useTimer.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 const INTERVAL: 1000 = 1000;
 
@@ -6,6 +6,7 @@ export default function useTimer(workoutTotalTime: number) {
   const [remainingTime, setRemainingTime] = useState(() => workoutTotalTime);
   const [isTimerRunning, setIsTimerRunning] = useState(false);
   const [isTimerDone, setIsTimerDone] = useState(false);
+  const timerIntervalRef = useRef<ReturnType<typeof setInterval> | null>(null);
 
   const toggleTimer = useCallback(() => {
     setIsTimerRunning(!isTimerRunning);
@@ -15,17 +16,17 @@ export default function useTimer(workoutTotalTime: number) {
     const currentTime = remainingTime - INTERVAL;
     setRemainingTime(currentTime);
     if (currentTime <= 0) {
-      console.log("Workout Done");
+      console.log("Workout Done", currentTime);
       setIsTimerRunning(false);
       setIsTimerDone(true);
     }
   };
 
   useEffect(() => {
-    let timerInterval: ReturnType<typeof setInterval>;
-    if (isTimerRunning) timerInterval = setInterval(updateTimer, INTERVAL);
-    return () => clearInterval(timerInterval);
-  }, [updateTimer, isTimerRunning]);
+    if (isTimerRunning)
+      timerIntervalRef.current = setInterval(updateTimer, INTERVAL);
+    return () => clearInterval(timerIntervalRef.current as NodeJS.Timeout);
+  }, [updateTimer]);
 
   return [remainingTime, isTimerRunning, isTimerDone, toggleTimer] as const;
 }
