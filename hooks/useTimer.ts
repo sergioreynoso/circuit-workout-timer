269de3ad7c11451e8b287cfd15e7e133a767fe28
Sergio Reynoso <sergior@gmail.com>
2022-12-18 15:06:00 -0500

    refactor: add useReducer to manage multiple state
    Code is more readable when updating multiple states at the same time.

--- hooks/useTimer.ts
+++ hooks/useTimer.ts
@@ -1,28 +1,69 @@
-import { useCallback, useState } from "react";
+import { useCallback, useReducer, useState } from "react";
 import useInterval from "./useInterval";
 
-const INTERVAL: 1000 = 1000;
+export const TIMER_INTERVAL: 1000 = 1000;
 
-export default function useTimer(workoutTotalTime: number) {
-  const [remainingTime, setRemainingTime] = useState(() => workoutTotalTime);
-  const [isTimerRunning, setIsTimerRunning] = useState(false);
-  const [isTimerDone, setIsTimerDone] = useState(false);
+type TimerState = {
+  runningTime: number;
+  isTicking: boolean;
+  isDone: boolean;
+};
+
+type TimerAction = {
+  type: "SET_DURATION" | "SET_IS_RUNNING" | "SET_IS_DONE";
+};
+
+const timerReducer = (state: TimerState, action: TimerAction) => {
+  switch (action.type) {
+    case "SET_DURATION":
+      return {
+        ...state,
+        runningTime: state.runningTime - TIMER_INTERVAL,
+      };
+    case "SET_IS_RUNNING":
+      return {
+        ...state,
+        isTicking: !state.isTicking,
+      };
+    case "SET_IS_DONE":
+      return {
+        ...state,
+        isTicking: false,
+        isDone: true,
+      };
+    default:
+      return state;
+  }
+};
+
+export default function useTimer(
+  workoutTotalTime: number,
+  callback: () => void
+) {
+  const [state, dispatch] = useReducer(timerReducer, {
+    runningTime: workoutTotalTime,
+    isTicking: false,
+    isDone: false,
+  });
 
   const toggleTimer = useCallback(() => {
-    setIsTimerRunning(!isTimerRunning);
-  }, [isTimerRunning]);
+    dispatch({ type: "SET_IS_RUNNING" });
+  }, []);
 
   const updateTimer = () => {
-    const currentTime = remainingTime - INTERVAL;
-    setRemainingTime(currentTime);
-    if (currentTime <= 0) {
-      console.log("Workout Done", currentTime);
-      setIsTimerRunning(false);
-      setIsTimerDone(true);
+    dispatch({ type: "SET_DURATION" });
+    if (state.runningTime <= 1000) {
+      console.log("Workout Done");
+      dispatch({ type: "SET_IS_DONE" });
     }
+    callback();
   };
 
-  useInterval(updateTimer, isTimerRunning ? INTERVAL : null);
-
-  return [remainingTime, isTimerRunning, isTimerDone, toggleTimer] as const;
+  useInterval(updateTimer, state.isTicking ? TIMER_INTERVAL : null);
+  return [
+    state.runningTime,
+    state.isTicking,
+    state.isDone,
+    toggleTimer,
+  ] as const;
 }
