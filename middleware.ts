2675bdb24354d1fe61813a84c10b9a18e8c24f10
Sergio Reynoso <sergior@gmail.com>
2023-01-18 12:25:35 -0500

    refactor: add multiple paths to api in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,6 @@
-export { default } from "next-auth/middleware";
+export { default } from 'next-auth/middleware';
 
 // // See "Matching Paths" below to learn more
 export const config = {
-  matcher: [
-    "/dashboard",
-    "/workout/:path*",
-    "/editWorkout/:path*",
-    "/api/workouts",
-    "/api/createWorkout",
-    "/api/updateExercise",
-    "/api/deleteWorkout",
-    "/api/createExercise",
-    "/api/deleteExercise",
-    "/api/updateExercise",
-    "/api/createExercise",
-  ],
+  matcher: ['/dashboard', '/workout/:path*', '/editWorkout/:path*', '/api/:path*'],
 };
