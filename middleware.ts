a1835334c89615f9cbd78bb1573c7f80abdece95
Sergio Reynoso <sergior@gmail.com>
2023-01-24 10:27:38 -0500

    refactor: update matcher paths

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
-export { default } from 'next-auth/middleware';
+export { default } from "next-auth/middleware";
 
-// // See "Matching Paths" below to learn more
+// See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/dashboard', '/workout/:path*', '/editWorkout/:path*', '/api/:path*'],
+  matcher: ["/dashboard", "/workout/:path*", "/editWorkout/:path*", "/api/:path*"],
 };
