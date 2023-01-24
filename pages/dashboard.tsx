39eb22ce9e2341bb3e2a1dd72c266075e9d6b02c
Sergio Reynoso <sergior@gmail.com>
2023-01-24 10:38:17 -0500

    refactor: remove SSR
    Loading data on the client side

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,123 +1,98 @@
-import { Exercise, Workout } from "@prisma/client";
-import { GetServerSideProps } from "next";
-import { unstable_getServerSession } from "next-auth";
-import { Container, Flex } from "../components/layout";
+import { useSession } from "next-auth/react";
+import { Container } from "../components/layout";
+import Preloader from "../components/preloader";
 import WorkoutList from "../components/workoutList";
 import WorkoutListHeader from "../components/workoutListHeader/workoutListHeader";
-import { prisma } from "../lib/prisma";
-import { authOptions } from "./api/auth/[...nextauth]";
 
-type DashboardProps = {
-  id: string;
-  initialData: Workout[];
-};
+const Dashboard = () => {
+  const session = useSession();
+  const userId = session.data?.user?.id;
 
-const exercises: Partial<Exercise>[] = [
-  {
-    exercise_name: "Squats",
-    display_seq: 0,
-    duration: 30000,
-    type: "EXERCISE",
-  },
-  {
-    exercise_name: "Sit-Ups",
-    display_seq: 1,
-    duration: 30000,
-    type: "EXERCISE",
-  },
-  {
-    exercise_name: "Lunges",
-    display_seq: 2,
-    duration: 30000,
-    type: "EXERCISE",
-  },
-  {
-    exercise_name: "Jumping Jacks",
-    display_seq: 3,
-    duration: 30000,
-    type: "EXERCISE",
-  },
-  {
-    exercise_name: "Plank",
-    display_seq: 4,
-    duration: 30000,
-    type: "EXERCISE",
-  },
-  {
-    exercise_name: "Mountain Climber",
-    display_seq: 5,
-    duration: 30000,
-    type: "EXERCISE",
-  },
-  {
-    exercise_name: "Pushups",
-    display_seq: 6,
-    duration: 30000,
-    type: "EXERCISE",
-  },
-  {
-    exercise_name: "Jumping Jacks",
-    display_seq: 7,
-    duration: 30000,
-    type: "EXERCISE",
-  },
-  {
-    exercise_name: "Burpee",
-    display_seq: 8,
-    duration: 30000,
-    type: "EXERCISE",
-  },
-];
+  if (!userId) {
+    return <Preloader label="Loading" />;
+  }
 
-const Dashboard = ({ id, initialData }: DashboardProps) => {
   return (
     <Container>
-      <WorkoutListHeader userId={id} />
-      <WorkoutList data={initialData as Workout[]} />
+      <WorkoutListHeader userId={userId} />
+      <WorkoutList userId={userId} />
     </Container>
   );
 };
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const session = await unstable_getServerSession(
-    context.req,
-    context.res,
-    authOptions
-  );
-  const id = session?.user?.id;
-  const workouts = await prisma?.workout.findMany({
-    where: {
-      userId: id as string,
-    },
-    orderBy: {
-      display_seq: "asc",
-    },
-  });
-
-  // if (workouts.length <= 0) {
-  //   const workout = await prisma?.workout.create({
-  //     data: {
-  //       workout_name: "Default workout",
-  //       set_count: 3,
-  //       set_rest: 3000,
-  //       userId: id,
-  //       display_seq: 0,
-  //       exercises: {
-  //         create: exercises,
-  //       },
-  //     },
-  //     include: {
-  //       exercises: true,
-  //     },
-  //   });
-  // }
-
-  return {
-    props: {
-      id,
-      initialData: workouts,
-    },
-  };
-};
-
 export default Dashboard;
+
+// if (workouts.length <= 0) {
+//   const workout = await prisma?.workout.create({
+//     data: {
+//       workout_name: "Default workout",
+//       set_count: 3,
+//       set_rest: 3000,
+//       userId: id,
+//       display_seq: 0,
+//       exercises: {
+//         create: exercises,
+//       },
+//     },
+//     include: {
+//       exercises: true,
+//     },
+//   });
+// }
+
+// const exercises: Partial<Exercise>[] = [
+//   {
+//     exercise_name: "Squats",
+//     display_seq: 0,
+//     duration: 30000,
+//     type: "EXERCISE",
+//   },
+//   {
+//     exercise_name: "Sit-Ups",
+//     display_seq: 1,
+//     duration: 30000,
+//     type: "EXERCISE",
+//   },
+//   {
+//     exercise_name: "Lunges",
+//     display_seq: 2,
+//     duration: 30000,
+//     type: "EXERCISE",
+//   },
+//   {
+//     exercise_name: "Jumping Jacks",
+//     display_seq: 3,
+//     duration: 30000,
+//     type: "EXERCISE",
+//   },
+//   {
+//     exercise_name: "Plank",
+//     display_seq: 4,
+//     duration: 30000,
+//     type: "EXERCISE",
+//   },
+//   {
+//     exercise_name: "Mountain Climber",
+//     display_seq: 5,
+//     duration: 30000,
+//     type: "EXERCISE",
+//   },
+//   {
+//     exercise_name: "Pushups",
+//     display_seq: 6,
+//     duration: 30000,
+//     type: "EXERCISE",
+//   },
+//   {
+//     exercise_name: "Jumping Jacks",
+//     display_seq: 7,
+//     duration: 30000,
+//     type: "EXERCISE",
+//   },
+//   {
+//     exercise_name: "Burpee",
+//     display_seq: 8,
+//     duration: 30000,
+//     type: "EXERCISE",
+//   },
+// ];
