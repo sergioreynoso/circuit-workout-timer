83acdc9d1f723aa65c8e4cf397ae796aa083d9bf
Sergio Reynoso <sergior@gmail.com>
2023-01-25 12:54:45 -0500

    refactor: change default isolationLevel
    Change isolationLevel to Serializable to make sure methods are serialized.

--- pages/api/v1/updateExerciseOrder.ts
+++ pages/api/v1/updateExerciseOrder.ts
@@ -1,5 +1,6 @@
 import { prisma } from "../../../lib/prisma";
 import { NextApiRequest, NextApiResponse } from "next";
+import { Prisma } from "@prisma/client";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const id = req.body.id;
@@ -7,18 +8,23 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   if (req.method === "POST") {
     try {
-      await prisma.$transaction([
-        prisma.exercise.deleteMany({
-          where: {
-            workout: {
-              id: id,
+      await prisma.$transaction(
+        [
+          prisma.exercise.deleteMany({
+            where: {
+              workout: {
+                id: id,
+              },
             },
-          },
-        }),
-        prisma.exercise.createMany({
-          data: data,
-        }),
-      ]);
+          }),
+          prisma.exercise.createMany({
+            data: data,
+          }),
+        ],
+        {
+          isolationLevel: Prisma.TransactionIsolationLevel.Serializable, // optional, default defined by database configuration
+        }
+      );
 
       res.status(200).json({ status: "success", message: "Updated activities" });
     } catch (error) {
