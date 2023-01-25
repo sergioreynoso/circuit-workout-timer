c4ed8572e035e77ac3f021368880df24b265f6d9
Sergio Reynoso <sergior@gmail.com>
2023-01-25 11:49:20 -0500

    refactor: add one to index
    List order should start with from 1 and not 0.

--- lib/updateDisplaySeq.ts
+++ lib/updateDisplaySeq.ts
@@ -2,7 +2,7 @@ import { Exercise, Workout } from "@prisma/client";
 
 const updateDisplaySeq = <T extends Exercise | Workout>(items: T[]): T[] => {
   return items.map((item, index) => {
-    return { ...item, display_seq: index };
+    return { ...item, display_seq: index + 1 };
   });
 };
 
