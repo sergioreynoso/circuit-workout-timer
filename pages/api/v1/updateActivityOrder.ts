15f7e2c715282bfa2e78c4088bb9d9ba15be1e01
Sergio Reynoso <sergior@gmail.com>
2023-02-09 12:19:50 -0500

    refactor: add http method error handling

--- pages/api/v1/updateActivityOrder.ts
+++ pages/api/v1/updateActivityOrder.ts
@@ -1,34 +1,37 @@
 import { prisma } from '../../../lib/prisma';
 import { NextApiRequest, NextApiResponse } from 'next';
-import { Prisma } from '@prisma/client';
+import { Activity, Prisma } from '@prisma/client';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const id = req.body.id;
-  const data = req.body.activity;
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'PATCH') {
+    updateWorkoutOrder(req, res);
+  } else {
+    res.setHeader('Allow', 'PATCH');
+    res.status(405).json({ status: 'error', message: `${req.method} method not allowed` });
+  }
+}
 
-  if (req.method === 'POST') {
-    try {
-      await prisma.$transaction(
-        [
-          prisma.activity.deleteMany({
-            where: {
-              workout: {
-                id: id,
-              },
-            },
-          }),
-          prisma.activity.createMany({
-            data: data,
-          }),
-        ],
-        {
-          isolationLevel: Prisma.TransactionIsolationLevel.Serializable,
-        }
-      );
-
-      res.status(200).json({ status: 'success', message: 'Updated activities' });
-    } catch (error) {
-      res.status(400).json({ status: 'failed', message: 'Something went wrong updating workout.' });
-    }
+async function updateWorkoutOrder(req: NextApiRequest, res: NextApiResponse) {
+  const activities = req.body;
+  try {
+    await prisma.$transaction(
+      await activities.map((activity: Activity) => {
+        return prisma.activity.update({
+          where: {
+            id: activity.id,
+          },
+          data: {
+            display_seq: activity.display_seq,
+          },
+        });
+      }),
+      {
+        isolationLevel: Prisma.TransactionIsolationLevel.Serializable,
+      }
+    );
+    res.status(200).json({ status: 'success', message: 'Updated activities order' });
+  } catch (error) {
+    console.log('REQUEST ERROR:', error);
+    res.status(500).json({ status: 'error', message: 'Error updating activities order' });
   }
 }
--- pages/api/v1/updateWorkoutOrder.ts
+++ pages/api/v1/updateWorkoutOrder.ts
@@ -1,32 +1,37 @@
 import { prisma } from '../../../lib/prisma';
 import { NextApiRequest, NextApiResponse } from 'next';
-import { Prisma } from '@prisma/client';
+import { Prisma, Workout } from '@prisma/client';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { id, data } = req.body;
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'PATCH') {
+    updateWorkoutOrder(req, res);
+  } else {
+    res.setHeader('Allow', 'PATCH');
+    res.status(405).json({ status: 'error', message: `${req.method} method not allowed` });
+  }
+}
 
-  if (req.method === 'POST') {
-    try {
-      await prisma.$transaction(
-        [
-          prisma.workout.deleteMany({
-            where: {
-              user: {
-                id: id,
-              },
-            },
-          }),
-          prisma.workout.createMany({
-            data: data,
-          }),
-        ],
-        {
-          isolationLevel: Prisma.TransactionIsolationLevel.Serializable,
-        }
-      );
-      res.status(200).json({ status: 'success', message: 'Updated activities' });
-    } catch (error) {
-      res.status(400).json({ status: 'failed', message: error });
-    }
+async function updateWorkoutOrder(req: NextApiRequest, res: NextApiResponse) {
+  const workouts = req.body;
+  try {
+    await prisma.$transaction(
+      await workouts.map((workout: Workout) => {
+        return prisma.workout.update({
+          where: {
+            id: workout.id,
+          },
+          data: {
+            display_seq: workout.display_seq,
+          },
+        });
+      }),
+      {
+        isolationLevel: Prisma.TransactionIsolationLevel.Serializable,
+      }
+    );
+    res.status(200).json({ status: 'success', message: 'Updated workout order' });
+  } catch (error) {
+    console.log('REQUEST ERROR:', error);
+    res.status(500).json({ status: 'error', message: 'Error updating workout order' });
   }
 }
