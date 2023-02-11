7c84a3712b5bad6a73751ebd18924a21bef50ba2
Sergio Reynoso <sergior@gmail.com>
2023-02-10 21:38:50 -0500

    refactor: add sort api routes

--- lib/endPoints.ts
+++ lib/endPoints.ts
@@ -3,8 +3,10 @@ const ver = 'v1';
 export const endPoints = {
   workouts: `/api/${ver}/workouts`,
   workout: `/api/${ver}/workout`,
+  workoutSort: `/api/${ver}/updateWorkoutOrder`,
   activities: `/api/${ver}/activities`,
   activity: `/api/${ver}/activity`,
+  activitySort: `/api/${ver}/updateActivityOrder`,
 };
 
 export type EndPoints = keyof typeof endPoints;
