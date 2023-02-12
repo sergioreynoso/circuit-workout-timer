0d38faa69cf570573319915f9288ae3fc463512c
Sergio Reynoso <sergior@gmail.com>
2023-02-11 22:23:22 -0500

    refactor: add key to force remount
    Data is passed to component state.

--- components/workouts/workouts.tsx
+++ components/workouts/workouts.tsx
@@ -1,25 +1,23 @@
 import { Workout } from '@prisma/client';
 import { useQuery } from '@tanstack/react-query';
-import React from 'react';
 import fetcher from '../../lib/fetcher';
-import { Container } from '../layout';
 import Preloader from '../preloader/preloader';
 import WorkoutListHeader from '../workoutListHeader';
 import WorkoutSortableList from '../workoutSortableList';
 
 const Workouts = ({ userId }: { userId: string }) => {
-  const { status, data, error } = useQuery({
+  const query = useQuery({
     queryKey: ['workouts'],
     queryFn: () => fetcher<Workout[]>(userId, 'workouts'),
   });
 
-  if (status !== 'success' && !data) return <Preloader label="Loading workouts..." />;
-  if (error) return <Preloader label="Error loading page" />;
+  if (query.status !== 'success' && !query.data) return <Preloader label="Loading workouts..." />;
+  if (query.error) return <Preloader label="Error loading page" />;
 
   return (
     <>
       <WorkoutListHeader userId={userId} />
-      <WorkoutSortableList data={data} />
+      <WorkoutSortableList key={query.data.length} data={query.data} />
     </>
   );
 };
