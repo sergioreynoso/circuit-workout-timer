10b2b36d336ed3824425294b8e40e50be9492b81
Sergio Reynoso <sergior@gmail.com>
2023-03-01 10:38:35 -0500

    refactor: update styles
    Button needs to have smaller padding

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -1,38 +1,39 @@
-import { ReactNode } from 'react';
+import { ComponentPropsWithoutRef, ReactNode } from 'react';
 
-type Props = {
+const variant = {
+  primary: {
+    text: 'text-amber-900',
+    bg: 'bg-amber-400',
+    hover: {
+      text: 'hover:text-amber-900',
+      bg: 'hover:bg-amber-300',
+    },
+  },
+  secondary: {
+    text: 'text-green-800',
+    bg: 'bg-green-400',
+    hover: {
+      text: 'hover:text-green-800',
+      bg: 'hover:bg-green-300',
+    },
+  },
+} as const;
+
+interface Props extends ComponentPropsWithoutRef<'button'> {
   children: ReactNode;
-  intent: 'primary' | 'secondary';
-};
+  intent: keyof typeof variant;
+}
 
-function Button({ intent, children, ...delegated }: Props) {
-  const variant = {
-    primary: {
-      text: 'text-amber-900',
-      bg: 'bg-amber-400',
-      hover: {
-        text: 'hover:text-amber-900',
-        bg: 'hover:bg-amber-300',
-      },
-    },
-    secondary: {
-      text: 'text-green-800',
-      bg: 'bg-green-400',
-      hover: {
-        text: 'hover:text-green-800',
-        bg: 'hover:bg-green-300',
-      },
-    },
-  };
-
+const Button = ({ intent = 'primary', children, ...props }: Props) => {
   return (
     <button
-      className={`h-16 flex-grow rounded-2xl ${variant[intent].text} ${variant[intent].bg} text-xl font-extrabold leading-7  ${variant[intent].hover.bg} ${variant[intent].hover.text}`}
-      {...delegated}
+      className={`
+     flex h-14 items-center justify-center gap-2 rounded-2xl px-4 sm:px-8 ${variant[intent].text} ${variant[intent].bg} text-md font-bold leading-7  ${variant[intent].hover.bg} ${variant[intent].hover.text}`}
+      {...props}
     >
       {children}
     </button>
   );
-}
+};
 
 export default Button;
