f44cb7f264eceedeef24c0f2a2d08c7882988a8c
Sergio Reynoso <sergior@gmail.com>
2023-03-02 13:10:24 -0500

    refactor: remove maxLength attribute

--- components/input/input.tsx
+++ components/input/input.tsx
@@ -13,12 +13,7 @@ const Input = ({ label, ...delegated }: Props) => {
       <Label.Root htmlFor={id} className="text-base font-semibold leading-6 text-gray-300">
         {label}
       </Label.Root>
-      <input
-        id={id}
-        {...delegated}
-        maxLength={18}
-        className="max-w h-12 rounded-lg border border-gray-500 bg-transparent px-4"
-      />
+      <input id={id} {...delegated} className="max-w h-12 rounded-lg border border-gray-500 bg-transparent px-4" />
     </div>
   );
 };
