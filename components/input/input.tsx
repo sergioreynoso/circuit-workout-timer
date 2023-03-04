500d7d9e4f0c8abdaa5e8746c8f341bd4a77aef6
Sergio Reynoso <sergior@gmail.com>
2023-03-03 19:22:22 -0500

    fix: input did not focus on load
    Added autoFocus attribute

--- components/input/input.tsx
+++ components/input/input.tsx
@@ -1,6 +1,6 @@
 import * as Label from '@radix-ui/react-label';
 
-import React, { useId } from 'react';
+import React, { forwardRef, useId } from 'react';
 
 interface Props extends React.ComponentPropsWithoutRef<'input'> {
   label: string;
@@ -10,12 +10,36 @@ const Input = ({ label, ...delegated }: Props) => {
   const id = useId();
   return (
     <div className="flex w-full flex-col gap-3">
-      <Label.Root htmlFor={id} className="text-base font-semibold leading-6 text-gray-300">
+      <Label.Root htmlFor={id} className="text-base font-normal leading-6 text-gray-300">
         {label}
       </Label.Root>
-      <input id={id} {...delegated} className="max-w h-12 rounded-lg border border-gray-500 bg-transparent px-4" />
+      <input
+        id={id}
+        {...delegated}
+        className="max-w h-12 rounded-lg border border-gray-500 bg-transparent px-4"
+        autoFocus
+      />
     </div>
   );
 };
+
+//TODO: Fix type errors
+// const Input = forwardRef(({ label, ...props }: Props, forwardedRef) => {
+//   const id = useId();
+//   return (
+//     <div className="flex w-full flex-col gap-3">
+//       <Label.Root htmlFor={id} className="text-base font-normal leading-6 text-gray-300">
+//         {label}
+//       </Label.Root>
+//       <input
+//         id={id}
+//         {...props}
+//         className="max-w h-12 rounded-lg border border-gray-500 bg-transparent px-4"
+//         ref={forwardedRef}
+//         autoFocus
+//       />
+//     </div>
+//   );
+// });
 
 export default Input;
