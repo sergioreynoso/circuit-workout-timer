596247e54e0f2c19d958bff56703dc94686751fa
Sergio Reynoso <sergior@gmail.com>
2023-03-03 19:23:03 -0500

    Feat: add slicer to change duration

--- components/addActivityDialog/addActivityDialog.tsx
+++ components/addActivityDialog/addActivityDialog.tsx
@@ -1,14 +1,17 @@
 import * as AlertDialogPrimitive from '@radix-ui/react-alert-dialog';
-import { Cancel, Title } from '@radix-ui/react-alert-dialog';
+import * as Label from '@radix-ui/react-label';
+import { Cancel } from '@radix-ui/react-alert-dialog';
 import { PlusIcon } from '@radix-ui/react-icons';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import axios from 'axios';
 import React, { useState } from 'react';
 import { FormattedActivity } from '../../hooks/useFormatWorkout';
+import { formatTime } from '../../lib/formatTime';
 import { WorkoutWithActivities } from '../../types/workout';
 import AlertDialog from '../alertDialog/alertDialog';
 import Button from '../button/button';
 import Input from '../input';
+import Slider from '../slider/slider';
 
 type Props = {
   data: WorkoutWithActivities;
@@ -18,7 +21,7 @@ const AddActivityDialog = ({ data }: Props) => {
   const [isOpen, setIsOpen] = useState(false);
   const [{ name, duration, workoutId }, setInputValue] = useState({
     name: '',
-    duration: 30,
+    duration: [30000],
     workoutId: data.id,
   });
 
@@ -48,7 +51,7 @@ const AddActivityDialog = ({ data }: Props) => {
     mutation.mutate({
       name: name,
       type: 'WORK',
-      duration: Number(duration * 1000),
+      duration: duration[0],
       workoutId: workoutId,
       display_seq: data.activities.length + 1,
     });
@@ -60,18 +63,30 @@ const AddActivityDialog = ({ data }: Props) => {
         <button className="text-md flex h-14 items-center gap-2 rounded-lg bg-green-500 px-4 font-bold leading-7 text-green-900 hover:bg-green-400 sm:px-8">
           <PlusIcon className="h-7 w-7" /> Add Activity
         </button>
+
+        {
+          //TODO: Solve ref bug.
+          /* <Button>
+          {' '}
+          <PlusIcon className="h-7 w-7" /> Add Activity
+        </Button> */
+        }
       </AlertDialogPrimitive.Trigger>
     );
   }
 
   return (
-    <AlertDialog TriggerButton={TriggerButton} isOpen={isOpen} setIsOpen={setIsOpen}>
-      <div className="items flex flex-col p-2 sm:p-4">
-        <Title className="mb-4 text-xl font-semibold leading-7 text-gray-300">Add Activity</Title>
-        <form onSubmit={onFormSubmit} className="flex flex-col items-start justify-center gap-4  ">
+    <AlertDialog TriggerButton={TriggerButton} isOpen={isOpen} setIsOpen={setIsOpen} title="Add Activity">
+      <div className="flex flex-col p-6">
+        {mutation.isLoading && (
+          <div className="absolute top-0 bottom-0 left-0 right-0 z-10 flex items-center justify-center bg-gray-900/95 ">
+            <p className="text-xl font-bold text-gray-300">Creating New Activity...</p>{' '}
+          </div>
+        )}
+        <form onSubmit={onFormSubmit} className="mt-2 flex flex-grow flex-col items-start gap-8">
           <Input
             type="text"
-            label="Exercise Name"
+            label="Name"
             name="name"
             value={name}
             onChange={handleChange}
@@ -79,16 +94,20 @@ const AddActivityDialog = ({ data }: Props) => {
             required={true}
             autoComplete="off"
           />
-          <Input
-            type="number"
-            label="Duration in seconds"
-            name="duration"
-            value={duration}
-            onChange={handleChange}
-            placeholder=""
-            required={true}
-          />
-          <div>{mutation.isLoading && 'Updating exercise...'}</div>
+          <div className="w-full ">
+            <Label.Root className="w-full text-base font-normal leading-6 text-gray-300">{'Duration'}</Label.Root>
+            <div className="mt-2 flex items-center gap-8">
+              <p className="w-16 text-end text-2xl font-bold text-green-500">{formatTime(duration[0])}</p>
+              <Slider
+                defaultValue={50000}
+                min={5000}
+                max={300000}
+                step={1000}
+                value={duration}
+                onValueChange={(value: number[]) => setInputValue(prev => ({ ...prev, duration: value }))}
+              />
+            </div>
+          </div>
           <div className="flex w-full justify-end gap-4">
             <Cancel asChild>
               <Button intent="transparent">Cancel</Button>
