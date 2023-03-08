b484c1dad2c5ac9dc3eff380a662a8b532fa0331
Sergio Reynoso <sergior@gmail.com>
2023-03-08 12:01:04 -0500

    add disable prop

--- components/slider/slider.tsx
+++ components/slider/slider.tsx
@@ -7,11 +7,12 @@ type Props = {
   max?: number;
   step?: number;
   value: number[];
+  disabled?: boolean;
   onValueChange?(value: number[]): void;
   onValueCommit?(value: number[]): void;
 };
 
-const Slider = ({ defaultValue = 50, min = 1, max = 100, step = 1, ...props }: Props) => {
+const Slider = ({ defaultValue = 50, min = 1, max = 100, step = 1, disabled = false, ...props }: Props) => {
   return (
     <SliderPrimitive.Root
       defaultValue={[defaultValue]}
@@ -20,9 +21,10 @@ const Slider = ({ defaultValue = 50, min = 1, max = 100, step = 1, ...props }: P
       step={step}
       aria-label="Volume"
       className="relative flex h-6 flex-grow touch-none select-none items-center"
+      disabled={disabled}
       {...props}
     >
-      <SliderPrimitive.Track className="relative h-0.5 flex-grow rounded-full bg-gray-900">
+      <SliderPrimitive.Track className="relative h-0.5 flex-grow rounded-full bg-gray-700">
         <SliderPrimitive.Range className="absolute h-full rounded-full bg-gray-300" />
       </SliderPrimitive.Track>
       <SliderPrimitive.Thumb className="block h-5 w-5 rounded-full bg-gray-100 shadow-md focus:shadow-sm" />
