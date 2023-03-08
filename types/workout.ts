728cd8c8337fb5745192d0a6b3658224941a8a21
Sergio Reynoso <sergior@gmail.com>
2023-03-08 12:00:45 -0500

    refactor: update WorkoutWithActivities type

--- types/workout.ts
+++ types/workout.ts
@@ -1,3 +1,8 @@
 import { Prisma } from '@prisma/client';
+import { Workout } from '@prisma/client';
+import { FormattedActivity } from '../lib/formatWorkout';
 
-export type WorkoutWithActivities = Prisma.WorkoutGetPayload<{ include: { activities: true } }>;
+// export type WorkoutWithActivities = Prisma.WorkoutGetPayload<{ include: { activities: true } }>;
+export interface WorkoutWithActivities extends Workout {
+  activities: FormattedActivity[];
+}
