f6b528f418547153563d6eb0f63d7a64d0a02594
Sergio Reynoso <sergior@gmail.com>
2023-03-10 14:59:01 -0500

    refactor: reformat default workouts on load

--- lib/defaultWorkout.ts
+++ lib/defaultWorkout.ts
@@ -2,6 +2,9 @@ import { Activity } from '@prisma/client';
 import { Optional } from 'ts-toolbelt/out/Object/Optional';
 import { FormattedActivity } from './formatWorkout';
 
+export const newActivity: Omit<FormattedActivity, 'id'>[] = one();
+// export const newActivity: Omit<FormattedActivity, 'id'>[] = kettleBell();
+
 export const defaultActivities: Omit<FormattedActivity, 'id'>[] = [
   {
     name: 'Jumping Jacks',
@@ -29,11 +32,66 @@ export const defaultActivities: Omit<FormattedActivity, 'id'>[] = [
   },
 ];
 
-export const newActivity: Omit<FormattedActivity, 'id'>[] = [
-  {
-    name: 'First Exercise',
-    display_seq: 0,
-    duration: 30000,
-    type: 'WORK',
-  },
-];
+function kettleBell(): Omit<FormattedActivity, 'id'>[] {
+  return [
+    {
+      name: 'Slingshot clockwise',
+      display_seq: 0,
+      duration: 30000,
+      type: 'WORK',
+    },
+    {
+      name: 'Single Arm Dead-Lift Left',
+      display_seq: 1,
+      duration: 30000,
+      type: 'WORK',
+    },
+    {
+      name: 'Slingshot counter-clockwise',
+      display_seq: 2,
+      duration: 30000,
+      type: 'WORK',
+    },
+    {
+      name: 'Single Arm Dead-Lift Right',
+      display_seq: 3,
+      duration: 30000,
+      type: 'WORK',
+    },
+    {
+      name: 'Slingshot clockwise',
+      display_seq: 4,
+      duration: 30000,
+      type: 'WORK',
+    },
+    {
+      name: 'Halo Alternate',
+      display_seq: 5,
+      duration: 30000,
+      type: 'WORK',
+    },
+    {
+      name: 'Slingshot counter-clockwise',
+      display_seq: 6,
+      duration: 30000,
+      type: 'WORK',
+    },
+    {
+      name: 'Goblet Squats',
+      display_seq: 7,
+      duration: 30000,
+      type: 'WORK',
+    },
+  ];
+}
+
+function one(): Omit<FormattedActivity, 'id'>[] {
+  return [
+    {
+      name: 'First Exercise',
+      display_seq: 0,
+      duration: 30000,
+      type: 'WORK',
+    },
+  ];
+}
