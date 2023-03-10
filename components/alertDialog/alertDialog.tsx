cd5045adc1e207501b311166d735d4805ed5abac
Sergio Reynoso <sergior@gmail.com>
2023-03-10 11:13:29 -0500

    fix: circle buttons bg not visible on modal back color
    Lighten modal bg color and darken overlay

--- components/alertDialog/alertDialog.tsx
+++ components/alertDialog/alertDialog.tsx
@@ -14,9 +14,9 @@ const AlertDialog = ({ TriggerButton, children, isOpen, setIsOpen, title = 'Titl
     <AlertDialogPrimitive.Root open={isOpen} onOpenChange={setIsOpen}>
       <TriggerButton />
       <AlertDialogPrimitive.Portal>
-        <AlertDialogPrimitive.Overlay className="fixed inset-0 bg-gray-900/80" />
-        <AlertDialogPrimitive.Content className="fixed top-1/2 left-1/2 w-[90vw] max-w-lg -translate-x-1/2 -translate-y-1/2 rounded-md bg-gray-800 sm:w-full">
-          <AlertDialogPrimitive.Title className="p-4 text-xl font-semibold leading-7 text-gray-300">
+        <AlertDialogPrimitive.Overlay className="fixed inset-0 bg-black/50" />
+        <AlertDialogPrimitive.Content className="fixed top-1/2 left-1/2 w-[90vw] max-w-lg -translate-x-1/2 -translate-y-1/2 rounded-md bg-gray-900 sm:w-full">
+          <AlertDialogPrimitive.Title className="px-5 py-4 text-xl font-semibold leading-7 text-gray-300">
             {title}
           </AlertDialogPrimitive.Title>
           <Separator.Root className="h-px bg-gray-600" decorative />
