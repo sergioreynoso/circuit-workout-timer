0cdf1028aea3ad694ba7cb8ef1b5e0480453cc54
Sergio Reynoso <sergior@gmail.com>
2023-03-15 22:14:32 -0400

    refactor: rename param
    Adds more clarity as to what data is the param.

--- hooks/reactQueryHooks/useFetchWorkout.ts
+++ hooks/reactQueryHooks/useFetchWorkout.ts
@@ -1,11 +1,16 @@
 import { useQuery } from '@tanstack/react-query';
+import { useSession } from 'next-auth/react';
 import fetcher from '../../lib/fetcher';
 import { WorkoutWithActivities } from '../../types/workout';
 
-export default function useFetchWorkout(id: string) {
+export default function useFetchWorkout(workoutId: string) {
+  const session = useSession();
+
+  console.log(session);
+
   return useQuery({
-    queryKey: ['workout', id],
-    queryFn: () => (id ? fetcher<WorkoutWithActivities>(id, 'workout') : null),
+    queryKey: ['workout', workoutId],
+    queryFn: () => fetcher<WorkoutWithActivities>(workoutId, 'workout'),
     refetchOnWindowFocus: false,
     cacheTime: Infinity,
   });
