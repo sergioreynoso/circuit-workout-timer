c92b2f20cab662e45484129d16cda0a03a840269
Sergio Reynoso <sergior@gmail.com>
2023-03-15 22:01:14 -0400

    refactor: create a default workout
    This workout will be loaded for first time users.

--- lib/defaultWorkout.ts
+++ lib/defaultWorkout.ts
@@ -1,97 +1,12 @@
-import { Activity } from '@prisma/client';
-import { Optional } from 'ts-toolbelt/out/Object/Optional';
+import { defaultActivities } from './defaultActivities';
 import { FormattedActivity } from './formatWorkout';
 
-export const newActivity: Omit<FormattedActivity, 'id'>[] = one();
-// export const newActivity: Omit<FormattedActivity, 'id'>[] = kettleBell();
-
-export const defaultActivities: Omit<FormattedActivity, 'id'>[] = [
-  {
-    name: 'Jumping Jacks',
-    display_seq: 0,
-    duration: 30000,
-    type: 'WORK',
-  },
-  {
-    name: 'Squats',
-    display_seq: 1,
-    duration: 30000,
-    type: 'WORK',
-  },
-  {
-    name: 'Pushups',
-    display_seq: 2,
-    duration: 30000,
-    type: 'WORK',
-  },
-  {
-    name: 'Plank',
-    display_seq: 3,
-    duration: 30000,
-    type: 'WORK',
-  },
-];
-
-function kettleBell(): Omit<FormattedActivity, 'id'>[] {
-  return [
-    {
-      name: 'Slingshot clockwise',
-      display_seq: 0,
-      duration: 30000,
-      type: 'WORK',
-    },
-    {
-      name: 'Single Arm Dead-Lift Left',
-      display_seq: 1,
-      duration: 30000,
-      type: 'WORK',
-    },
-    {
-      name: 'Slingshot counter-clockwise',
-      display_seq: 2,
-      duration: 30000,
-      type: 'WORK',
-    },
-    {
-      name: 'Single Arm Dead-Lift Right',
-      display_seq: 3,
-      duration: 30000,
-      type: 'WORK',
-    },
-    {
-      name: 'Slingshot clockwise',
-      display_seq: 4,
-      duration: 30000,
-      type: 'WORK',
-    },
-    {
-      name: 'Halo Alternate',
-      display_seq: 5,
-      duration: 30000,
-      type: 'WORK',
-    },
-    {
-      name: 'Slingshot counter-clockwise',
-      display_seq: 6,
-      duration: 30000,
-      type: 'WORK',
-    },
-    {
-      name: 'Goblet Squats',
-      display_seq: 7,
-      duration: 30000,
-      type: 'WORK',
-    },
-  ];
-}
-
-function one(): Omit<FormattedActivity, 'id'>[] {
-  return [
-    {
-      name: 'First Exercise',
-      display_seq: 0,
-      duration: 30000,
-      type: 'WORK',
-    },
-  ];
-}
+export const defaultWorkout = (userId: string) => ({
+  name: 'Light Workout',
+  set_count: 1,
+  set_rest: 10000,
+  duration: 125000,
+  userId: userId,
+  display_seq: 0,
+  activities: defaultActivities as FormattedActivity[],
+});
