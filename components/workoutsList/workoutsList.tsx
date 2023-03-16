051ac63e805390d8457183703b8c205c839d15ae
Sergio Reynoso <sergior@gmail.com>
2023-03-15 22:13:26 -0400

    refactor: implement custom fetch hook

--- components/workoutsList/workoutsList.tsx
+++ components/workoutsList/workoutsList.tsx
@@ -1,31 +1,12 @@
-import { Workout } from '@prisma/client';
-import { useQuery, useQueryClient } from '@tanstack/react-query';
-import useWorkoutMutation from '../../hooks/reactQueryHooks/useWorkoutMutation';
-import { defaultWorkout } from '../../lib/defaultWorkout';
-import fetcher from '../../lib/fetcher';
+import useFetchWorkoutList from '../../hooks/reactQueryHooks/useFetchWorkoutList';
 import Preloader from '../preloader/preloader';
 import WorkoutListHeader from '../workoutListHeader';
 import WorkoutSortableList from '../workoutSortableList';
 
 const Workouts = ({ userId }: { userId: string }) => {
-  const { createWorkout } = useWorkoutMutation('');
+  const query = useFetchWorkoutList(userId);
 
-  const queryClient = useQueryClient();
-  const query = useQuery({
-    queryKey: ['workouts'],
-    queryFn: () => fetcher<Workout[]>(userId, 'workouts'),
-    onSuccess: data => {
-      if (data.length === 0) {
-        createWorkout.mutate(defaultWorkout(userId), {
-          onSuccess: () => {
-            queryClient.invalidateQueries(['workouts']);
-          },
-        });
-      }
-    },
-  });
-
-  if (query.status !== 'success' && !query.data) return <Preloader label="Loading workouts..." />;
+  if (!query.data) return <Preloader label="Loading workouts..." />;
   if (query.error) return <Preloader label="Error loading page" />;
 
   return (
---
+++ hooks/reactQueryHooks/useFetchWorkoutList.ts
@@ -0,0 +1,31 @@
+import { Workout } from '@prisma/client';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { useSession } from 'next-auth/react';
+import { defaultWorkout } from '../../lib/defaultWorkout';
+import fetcher from '../../lib/fetcher';
+import { WorkoutWithActivities } from '../../types/workout';
+import useWorkoutMutation from './useWorkoutMutation';
+
+export default function useFetchWorkoutList(userId: string) {
+  const session = useSession();
+  const queryClient = useQueryClient();
+  const { createWorkout } = useWorkoutMutation('');
+
+  console.log(session);
+
+  return useQuery({
+    queryKey: ['workouts'],
+    queryFn: () => fetcher<Workout[]>(userId, 'workouts'),
+    onSuccess: data => {
+      if (data.length === 0) {
+        createWorkout.mutate(defaultWorkout(userId), {
+          onSuccess: () => {
+            queryClient.invalidateQueries(['workouts']);
+          },
+        });
+      }
+    },
+    refetchOnWindowFocus: false,
+    cacheTime: Infinity,
+  });
+}
