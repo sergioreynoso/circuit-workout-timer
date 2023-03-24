57a74cc6121d66fd1665ce4706a80788cfa5cb21
Sergio Reynoso <sergior@gmail.com>
2023-03-24 10:42:27 -0400

    refactor: add header to landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,18 @@
 import type { NextPage } from 'next';
-import { signIn } from 'next-auth/react';
-import Head from 'next/head';
 import Image from 'next/image';
-import Button from '../components/button/button';
+import { Header } from '../components/header';
 import mock from '../public/mock.png';
-
-const NEXTAUTH_URL = process.env.NEXTAUTH_URL;
 
 const Home: NextPage = () => {
   return (
-    <div className="flex flex-col justify-center">
-      <div className="flex flex-col items-center justify-center  gap-5 p-6 sm:gap-10">
-        <p className="text-center text-2xl font-black leading-10 text-gray-200 sm:text-4xl">Interval Workout App</p>
-        <Image src={mock} priority alt="workout out displayed on an mobile phone" className="w-56 sm:w-72" />
-        <Button
-          intent="primary"
-          onClick={() =>
-            signIn(undefined, {
-              callbackUrl: `${NEXTAUTH_URL}/dashboard`,
-            })
-          }
-        >
-          Log In
-        </Button>
+    <>
+      <Header />
+      <div className="flex flex-col justify-center">
+        <div className="flex flex-col items-center justify-center  gap-5 p-6 sm:gap-10">
+          <Image src={mock} priority alt="workout out displayed on an mobile phone" className="w-56 sm:w-72" />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
