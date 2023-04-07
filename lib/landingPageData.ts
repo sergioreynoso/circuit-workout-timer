ef09abd86d0f84e8f0ecd51abadeb468dfc0d9dd
Sergio Reynoso <sergior@gmail.com>
2023-04-06 23:30:25 -0400

    add landing page data obj

---
+++ lib/landingPageData.ts
@@ -0,0 +1,11 @@
+export const ladingPageData = {
+  title: 'Interval Workout App',
+  subtitle: 'To help you keep track of your work and rest periods during workouts.',
+  bullets: [
+    'Create customizable workouts with exercises, rests, and sets.',
+    'Track current and upcoming exercises during workout.',
+    'Monitor total workout time and remaining time.',
+    'Save your workouts in the cloud by creating a profile.',
+    'Voice prompt for current and up coming activity.',
+  ],
+};
