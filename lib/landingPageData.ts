945f3a654535e4d40059791fd986c4a802f0ebbb
Sergio Reynoso <sergior@gmail.com>
2023-06-01 12:12:01 -0400

    update copy

--- lib/landingPageData.ts
+++ lib/landingPageData.ts
@@ -1,6 +1,6 @@
 export const ladingPageData = {
-  title: 'Circuit Timer',
-  subtitle: 'To help you keep track of your work and rest periods during workouts.',
+  title: 'Circuit Workout Timer',
+  subtitle: 'The Circuit Workout Timer web app is designed to help users track their work and rest periods during circuit training. The app allows users to create customizable workouts with exercises, rests, and sets. Users can track their current and upcoming exercise during the workout and monitor total workout time and remaining time.',
   bullets: [
     'Create customizable workouts with exercises, rests, and sets.',
     'Track current and upcoming exercises during workout.',
